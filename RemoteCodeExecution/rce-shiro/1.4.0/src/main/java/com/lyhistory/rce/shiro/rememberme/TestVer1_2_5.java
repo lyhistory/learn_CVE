47cd2fa59c5ae41980d191732e5d91cade017957
Yue Liu <yue.liu@asiapacificex.com>
2020-01-28 14:07:08 +0800

    apply gitignore fix

--- RemoteCodeExecution/rce-shiro/1.4.0/src/main/java/com/lyhistory/rce/shiro/rememberme/TestVer1_2_5.java
+++
@@ -1,7 +0,0 @@
-package com.lyhistory.rce.shiro.rememberme;
-
-// https://issues.apache.org/jira/browse/SHIRO-550?jql=project%20%3D%20SHIRO%20AND%20fixVersion%20%3D%201.2.5
-// https://github.com/apache/shiro/commit/4d5bb000a7f3c02d8960b32e694a565c95976848
-public class TestVer1_2_5 {
-
-}
